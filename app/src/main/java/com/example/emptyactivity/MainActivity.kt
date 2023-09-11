a2f2c28ad657186f42a7e834ff46b822955c55b0
MalfinK <muhammadalfin1235678@gmail.com>
2023-09-11 11:18:42 +0700

    Pert 1 pake surface

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -4,40 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.emptyactivity.ui.theme.EmptyActivityTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EmptyActivityTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            Greeting(name = "Saya")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface (
+        color = Color.Yellow
+    ) {
+        Text(
+            text = "Hello $name!",
+            fontSize = 30.sp,
+            modifier = modifier.padding(8.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun GreetingPreview() {
     EmptyActivityTheme {
-        Greeting("Android")
+        Greeting("Windows")
     }
 }
